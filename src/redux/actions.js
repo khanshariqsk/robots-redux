93913cbf60f48280ca410f301b2d89e81a5cc001
khanatir786 <104580369+khanatir786@users.noreply.github.com>
2022-06-18 22:14:47 +0530

    Added redux-thunk

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,9 @@
-import { SEARCH_ROBOTS, UPDATE_ROBOTS, UPDATE_ROBOTS_FAILURE, UPDATE_ROBOTS_PENDING, UPDATE_ROBOTS_SUCCESS } from "./constants";
+import {
+  SEARCH_ROBOTS,
+  UPDATE_ROBOTS_FAILURE,
+  UPDATE_ROBOTS_PENDING,
+  UPDATE_ROBOTS_SUCCESS,
+} from "./constants";
 
 export const searchRobotsAction = (text) => {
   return {
